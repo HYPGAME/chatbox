bfc815c7da5cac3f9aafdcb2328d17a8a5c1c54e
yp.huang <yp.huang@trip.com>
2026-04-15 16:30:17 +0800

    feat: add manual release contract helpers

---
+++ internal/release/manual.go
@@ -0,0 +1,23 @@
+package release
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var versionPattern = regexp.MustCompile(`^v[0-9]+\.[0-9]+\.[0-9]+$`)
+
+func ValidateVersion(version string) error {
+	if !versionPattern.MatchString(version) {
+		return fmt.Errorf("invalid version %q: use vMAJOR.MINOR.PATCH", version)
+	}
+	return nil
+}
+
+func ArtifactNames() []string {
+	return []string{
+		"chatbox_darwin_arm64.tar.gz",
+		"chatbox_darwin_amd64.tar.gz",
+		"checksums.txt",
+	}
+}
---
+++ internal/release/manual_test.go
@@ -0,0 +1,41 @@
+package release
+
+import "testing"
+
+func TestValidateVersionAcceptsSemanticTag(t *testing.T) {
+	t.Parallel()
+
+	if err := ValidateVersion("v0.1.3"); err != nil {
+		t.Fatalf("ValidateVersion returned error: %v", err)
+	}
+}
+
+func TestValidateVersionRejectsInvalidFormats(t *testing.T) {
+	t.Parallel()
+
+	cases := []string{"0.1.3", "v1", "latest", "v1.2.3-rc1"}
+	for _, tc := range cases {
+		if err := ValidateVersion(tc); err == nil {
+			t.Fatalf("expected invalid version %q to fail", tc)
+		}
+	}
+}
+
+func TestArtifactNamesForVersionedRelease(t *testing.T) {
+	t.Parallel()
+
+	assets := ArtifactNames()
+	want := []string{
+		"chatbox_darwin_arm64.tar.gz",
+		"chatbox_darwin_amd64.tar.gz",
+		"checksums.txt",
+	}
+	if len(assets) != len(want) {
+		t.Fatalf("expected %d assets, got %d", len(want), len(assets))
+	}
+	for i := range want {
+		if assets[i] != want[i] {
+			t.Fatalf("expected asset %d to be %q, got %q", i, want[i], assets[i])
+		}
+	}
+}
